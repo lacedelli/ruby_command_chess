35fca920b349f227c0fa29b4f2db143fb57f6596
Lacedelli <lacedelli@outlook.com>
2020-05-03 11:46:28 -0500

    add cell class with basic interaction

---
+++ lib/cell.rb
@@ -0,0 +1,24 @@
+class Cell
+	attr_reader :piece, :coord
+	
+	def initialize(coordinates)
+		@piece = nil
+		@coord = coordinates
+	end
+
+	def remove_piece()
+		piece_container = @piece
+		@piece = nil
+		piece_container
+	end
+
+	def set_piece(piece)
+		@piece = piece
+		nil
+	end
+
+	private
+	attr_writer :piece, :coord
+
+end
+
