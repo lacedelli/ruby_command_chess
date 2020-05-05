0dcd18cbf07eff3d4aff0d6c5a2e1cdc5da39d01
Lacedelli <lacedelli@outlook.com>
2020-05-05 18:25:02 -0500

    add restructure tree, where pieces' moves get updated after each move

--- lib/board.rb
+++ lib/board.rb
@@ -18,30 +18,52 @@ class Board
 		end
 	end
 
-	def get_cell(coord)
-		@grid.each do |row|
-			if row[0].coord[0] == coord[0]
-				row.each do |cell|
-					if cell.coord[1] == coord[1]
-						return cell
-					end
+	def update_board()
+		@grid.each do |column|
+			column.map do |cell|
+				unless cell.contains_piece?()
+					next
+				else
+					# TODO update the move & threat values
+					# of cell.piece.move && cell.piece.threat
 				end
-			end	
-		end
-		nil
-	end
-
-	# Receives a coordinate hash from a piece and returns a hash of cells
-	def coord_to_cell(coord)
-		coord.each do |key, array|
-			puts "inside key #{key}"
-			counter = 0
-			array.each do |cell|
-				puts "inside cell #{cell}"
-				coord[key][counter] = get_cell(cell)
-				counter += 1
 			end
 		end
+	end
+
+	def make_move(instruction)
+		# TODO parse the instruction 
+		# first letter should be upper case
+		# if not, move affects a pawn
+		# 
+		# find starting coordinates
+		# find dash separator
+		# check for "x" regarding piece capturing
+		# find ending coordinates
+		#
+		# confirm that a piece exists in selected space
+		# confirm that piece is the type specified
+		# confirm that the movement the player asked is valid
+		# if x was found instead of dash, check for capture
+		# execute move if all conditions are true
+		#
+		# if move is not parseable
+		# or if move is not actionable, ask player to input again
+	end
+
+	private
+	attr_writer :grid
+
+	def move_piece(origin, destination)
+		# TODO use a variable to hold origin.piece
+		# set origin.piece to nil
+		# get cell on destination coordinates
+		# set destination.piece to piece
+	end
+
+	def capture_piece(origin, destination)
+		# TODO move destination.piece to a captured array
+		# call move_piece(origin, destination)
 	end
 
 end
