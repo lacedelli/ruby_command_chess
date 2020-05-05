e2f5ebf2c7738439c3169569c608fb7342aef722
Lacedelli <lacedelli@outlook.com>
2020-05-04 19:38:47 -0500

    add changes to the way the board was created for ease of traversal

--- lib/board.rb
+++ lib/board.rb
@@ -6,12 +6,25 @@ class Board
 	def initialize()
 		@grid = []
 		hor_range = ("a".."h")
-		8.times do |row|
+		counter = 0
+		hor_range.each do |col|
 			@grid << []
-			hor_range.each do |col|
-				@grid[row] << Cell.new([col, row + 1])
+			p col
+			8.times do |row|
+				p row + 1
+				@grid[counter] << Cell.new([col, row + 1])
+			end
+			counter += 1
+		end
+	end
+
+	def get_cell(coord)
+		@grid.each do |row|
+			row.each do |cell|
+				p cell.coord
 			end
 		end
+		nil
 	end
 
 end
