9fb2f2af79e8def2b3918ede9fc169c679728de6
Lacedelli <lacedelli@outlook.com>
2020-05-06 18:18:47 -0500

    add regexp to player input

--- lib/board.rb
+++ lib/board.rb
@@ -32,16 +32,34 @@ class Board
 	end
 
 	def make_move(instruction)
-		# TODO parse the instruction 
+		# Parse the instruction 
+		long_notation = /^([KQBNR]|[kqbnr])?([a-h][1-8])(-|x)([a-h][1-8])/
+		match = long_notation.match(instruction)
 		# first letter should be upper case
-		# if not, move affects a pawn
-		# 
-		# find starting coordinates
-		# find dash separator
-		# check for "x" regarding piece capturing
+		unless match[1].nil?
+			# Move affects pawn
+		else
+			# find starting coordinates
+			origin = get_cell(match[2])
+			if origin.nil?
+				# TODO find a way to tell player nature of error
+				# outside of board
+				return nil
+			end
+		# Find operand
+			operand = get_cell(match[3])
+			if operand.nil?
+				# TODO Tell player nature of error 
+				return nil 
+			end
 		# find ending coordinates
-		#
+			destination = get_cell(match[4])
+			if destination.nil?
+				# TODO Tell player nature of error
+				return nil
+			end
 		# confirm that a piece exists in selected space
+			unless origin.has_piece?
 		# confirm that piece is the type specified
 		# confirm that the movement the player asked is valid
 		# if x was found instead of dash, check for capture
@@ -49,6 +67,8 @@ class Board
 		#
 		# if move is not parseable
 		# or if move is not actionable, ask player to input again
+			end
+		end
 	end
 
 	private
@@ -59,11 +79,21 @@ class Board
 		# set origin.piece to nil
 		# get cell on destination coordinates
 		# set destination.piece to piece
+		# if piece == pawn
+		# if pawn first_move.true?
+		# set first move to false
 	end
 
 	def capture_piece(origin, destination)
 		# TODO move destination.piece to a captured array
 		# call move_piece(origin, destination)
+	end
+
+	def get_cell(coordinates)
+		# split coordinates into array
+		# TODO scan the whole board for the
+		# coordinates given, if found, return it
+		# otherwise return nil
 	end
 
 end
